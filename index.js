c5ab9df8fa407d2fd868fafe209a8594d77dd23a
gweltaz-calori <andoracontact@gmail.com>
2018-04-08 12:16:21 +0200

    better performances

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const {getFilePages,exportPages,getImagesContent,createPdf} = require('./utils')
+const pdfCreator = require('./utils')
 
 app.use(express.static('public'));
 
@@ -9,16 +9,17 @@ app.get('/',(req,res) => res.sendFile(path.resolve(__dirname,'./public/index.htm
 
 app.get('/export/:key', async(req, res) => {
     try {
-        let pages = await getFilePages(req.params.key)
-        let images = await exportPages(pages,req.params.key)
-        let svgContent = await getImagesContent(pages)
-        createPdf(pages,res)
+        let pages = await pdfCreator.getFilePages(req.params.key)
+        let images = await pdfCreator.exportPages(pages,req.params.key)
+        let svgContent = await pdfCreator.getImagesContent(pages)
+        pdfCreator.createPdf(pages,res)
         
 
         res.setHeader('Content-Type', 'application/pdf');
         
     }
     catch(e) {
+        console.log(e)
         res.status(400).send({
             message : "Invalid File"
         })
--- utils/index.js
+++ utils/index.js
@@ -1,9 +1,10 @@
 const axios = require('axios')
+const {performance}  = require('perf_hooks')
 const fs = require('fs')
 const blobStream = require('blob-stream');
 const PDFDocument  = require('pdfkit')
 const SVGtoPDF = require('svg-to-pdfkit');
-const ACCESS_TOKEN = "234-bf883247-6983-4b0b-af02-27733be96fda"
+const ACCESS_TOKEN = "<YOUR ACCESS TOKEN>"
 
 let axiosInstance = axios.create({
     baseURL : "https://api.figma.com/v1/",
@@ -11,28 +12,64 @@ let axiosInstance = axios.create({
         "X-Figma-Token" : ACCESS_TOKEN
     }
 })
-module.exports = {
+
+const pdfCreator = {
     async getFilePages(key) {
+        let start = performance.now()
         let response = await axiosInstance.get(`files/${key}`)
-        return response.data.document.children[0].children.sort((a,b) => parseInt(a.name) - parseInt(b.name))
+        let end = performance.now()
+        
+        console.log(`Got file pages in ${end - start} ms`)
+
+        console.log(response.data.document.children[0])
+
+        return response.data.document.children[0].children
+            .filter(layer => layer.type === "FRAME")
+            .sort((a,b) => parseInt(a.name) - parseInt(b.name))
     },
     async exportPages(pages,key) {
+        let start = performance.now()
+        let pagesPromises = []
+        
         for(let page of pages) {
-            let response = await axiosInstance.get(`images/${key}`,{
-                params : {
-                    ids : page.id,
-                    format : "svg"
-                }
-            })
-    
-            page.imageUrl = Object.values(response.data.images)[0]
+            let pagePromise = this.exportPage(page,key)
+            pagesPromises.push(pagePromise)
         }
+        await Promise.all(pagesPromises)
+        let end = performance.now()
+        
+        console.log(`Exported pages in ${end - start} ms `)
+
+        return "ok"
+    },
+    async exportPage(page,key) {
+        let response = await axiosInstance.get(`images/${key}`,{
+            params : {
+                ids : page.id,
+                format : "svg"
+            }
+        })
+
+        page.imageUrl = Object.values(response.data.images)[0]
+
+        return "exported"
     },
     async getImagesContent(pages) {
+        let start = performance.now()
+        let imagesPromises = []
         for(let page of pages ) {
-            let response = await axios.get(page.imageUrl)
-            page.svgContent = response.data
+            let imagePromise = this.getImageContent(page)
+            imagesPromises.push(imagePromise)
         }
+        await Promise.all(imagesPromises) 
+        let end = performance.now()
+        console.log(`Got images content in ${end - start} ms `)
+        return "ok"
+    },
+    async getImageContent(page) {
+        let response = await axios.get(page.imageUrl)
+        page.svgContent = response.data
+        return "content"
     },
     async createPdf(pages,res) {
         let doc = new PDFDocument({compress: false,size : [pages[0].absoluteBoundingBox.width * 0.75,pages[0].absoluteBoundingBox.height* 0.75]})
@@ -48,3 +85,5 @@ module.exports = {
         doc.end();
     }
 }
+
+module.exports = pdfCreator
